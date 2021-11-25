39d7c28d228bd1049dce8f7bcc618d0dae92bae2
spaul91 <sophie.paul@addenbrookes.nhs.uk>
2021-11-25 17:50:23 +0000

    fixes to app

--- src/run.sh
+++ src/run.sh
@@ -11,15 +11,17 @@ main() {
     dx-download-all-inputs --parallel
 
 
+    # B: INSTALL DEPENDENCIES
+    ## Install libffi-dev (dependency of _ctypes python module)
+    apt-get install ./libffi-dev_3.3-4_amd64.deb
 
-    # B: INSTALL PACKAGES
     ## Install python3
     ### Note: app entrypoint (i.e. starting directory)
     ###       will be "resources/home/dnanexus",
     ###       and "~" resolves to "/home/dnanexus"
     echo "Installing Python3..."
-    tar -xvzf Python-3.10.0.tar.xz
-    cd Python-3.*
+    tar xf Python-3.8.0.tar.xz
+    cd Python-3.8.0
     ./configure
     make install
     cd /home/dnanexus
@@ -28,19 +30,22 @@ main() {
     echo "Installing Python packages..."
 
     ### 1. Install wheel
-    pip3 install packages/wheel/wheel-0.37.0-py2.py3-none-any.whl
+    pip3 install packages/wheel/*.whl --no-deps --no-index
 
-    ### 2. Install TSO500Reporter dependencies
-    pip3 install packages/*.whl
+    ### 2. Upgrade pip
+    pip3 install packages/pip/*.whl --no-deps --no-index
 
-    ### 3. Install TSO500Reporter
-    pip3 install packages/tso500reporter/*.whl
+    ### 3. Install TSO500Reporter dependencies
+    pip3 install packages/*.whl --no-deps --no-index
+
+    ### 4. Install TSO500Reporter
+    pip3 install packages/tso500reporter/*.whl --no-deps --no-index
 
 
 
     # C: GENERATE REPORT
     echo "Running app..."
-    time python3 -m tso500reporter --variant-data *CombinedVariantOutput.tsv --samplesheet *.csv --output $output --pdf
+    time python3 -m tso500reporter --pdf --samplesheet $(find ~/in/ -name *.csv) --variant-data $(find ~/in/ -name *CombinedVariantOutput.tsv | paste -sd' ')
 
 
 
@@ -48,8 +53,8 @@ main() {
     echo "Completed. Uploading files..."
 
     ## Fetch output report files
-    html_report=$(find . report.html)
-    pdf_report=$(find . report.pdf)
+    html_report=$(find . -name report.html)
+    pdf_report=$(find . -name report.pdf)
 
     ## Upload files to DNANexus
     html_report_id=$(dx upload $html_report --brief)
